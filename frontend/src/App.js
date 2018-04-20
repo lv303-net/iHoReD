e46274cc7e212c6f3f8fee5e0284196367e2f53e
ira-halenok <iryna.halenok@gmail.com>
2018-04-21 00:38:45 +0300

    Changed hored-backend to hored (due to changing Web App)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ var server_url;
 if(process.env.NODE_ENV==="development")
   server_url="http://localhost:58511"
 else if(process.env.NODE_ENV==="production")
-  server_url="https://hored-backend.azurewebsites.net"
+  server_url="https://hored.azurewebsites.net"
 
 class App extends Component {
   render() {
