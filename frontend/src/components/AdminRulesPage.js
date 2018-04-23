527d78bec84e2d2f1621433b3b4cc7ea9bf49105
olegRakus <rakusoleg@gmail.com>
2018-04-23 17:59:00 +0300

    Added DoctorsListWithSomeRule which opened when chose some rule

--- frontend/src/components/AdminRulesPage.js
+++ frontend/src/components/AdminRulesPage.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import LogbarAuth from './LogbarAuth';
+import DoctorsListWithSomeRule from './DoctorsListWithSomeRule';
 
 class AdminRulesPage extends Component {
     render () {
@@ -20,46 +21,44 @@ class AdminRulesPage extends Component {
 }
 
 var server_url;
-if(process.env.NODE_ENV==="development")
+/*if(process.env.NODE_ENV==="development")
   server_url="http://localhost:58511"
-else if(process.env.NODE_ENV==="production")
+else if(process.env.NODE_ENV==="production")*/
   server_url="https://hored.azurewebsites.net"
 
 class RulesList extends Component {
     constructor() {
         super();
         this.state = {
-          }
-
+            idArr: [],
+            id: -1
+          };
+        this.eventHandler=this.eventHandler.bind(this);
         axios.get(server_url+'/rule')
         .then(res => {
-            res.data.forEach(rules => {
-              document.getElementById("listRules").innerHTML 
-               += '<button  class="list-group-item list-group-item-active d-flex justify-content-between" onclick={this.buttonClicked}>'
-               + rules.RuleName + '</button>';
-            });
+                this.setState({
+                  idArr: res.data
+                });
           });
       }
-buttonClicked() {
-    document.getElementById("listDoctors").innerHTML = <DoctorsListWithSomeRule/>;
-}
+
+      eventHandler(idP) {
+        localStorage.setItem("currentProfession", idP)
+        this.setState({
+          id: idP
+        })
+      }
 
     render() {
         return (
             <div className = "container">
-                <div className="list-group col-sm-6" id="listRules">
+                <div className="list-group col-sm-6 float-left" id="listRules">
+                {this.state.idArr.map(idr => <div className='list-group-item list-group-item-active' key={idr.toString()} onClick={() => this.eventHandler(idr.IdRule)}>{idr.RuleName}</div>)}
                 </div>
-                <div className="list-group col-sm-6" id="listDoctors">
+                <div id="listDoctors">
+                <DoctorsListWithSomeRule idRule={this.state.id}/>
                 </div>
             </div>
-        )
-    }
-}
-
-class DoctorsListWithSomeRule extends Component {
-    render () {
-        return (
-            <p>some</p>
         )
     }
 }
