52a69ae1884c972e865724e77a476145adcce063
Olya <solyarolya@gmail.com>
2018-04-23 19:53:32 +0300

    Created GetUserInfoById

--- backend/Entities/Services/IUserService.cs
+++ backend/Entities/Services/IUserService.cs
@@ -13,5 +13,6 @@ namespace Entities.Services
         User GetUserInfo(string email);
         void ApdateInfoAboutUser(string id, string firstname, string lastname, string email, string password, string isActivated,
             string phone, string sex, string country, string city, string street, string apartment);
+        UserInfo GetUserInfoById(int id);
     }
 }
