2cec3acca42361e9aea5c43fcd478a0b3d477f44
IvanovVadym <37081450+IvanovVadym@users.noreply.github.com>
2018-04-24 11:06:33 +0300

    Added name "Doctors"

--- frontend/src/components/DoctorsListWithSomeRule.js
+++ frontend/src/components/DoctorsListWithSomeRule.js
@@ -38,7 +38,9 @@ class DoctorsListWithSomeRule extends Component {
         this.getDoctors();
         return  (
                     <div className="col-sm-6 list-group mt-4" id = "doctors">
+                    <div className="list-group-item active bg-info">Doctors:</div>
                     {this.state.doc.map(doc => <div className='list-group-item list-group-active'key={doc.toString()}>{doc.FirstName + ' ' + doc.LastName}</div>)}                  
+                    </div>
                     </div>
                     );
     }
