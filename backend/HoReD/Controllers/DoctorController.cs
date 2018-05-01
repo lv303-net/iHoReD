a7886143cfe587d8c2152e682d7f8e5ecf10ead3
Сікорський Максим <uageneral@gmail.com>
2018-05-01 12:06:17 +0300

    Changed Controllers return type to IHttpActionResult

--- backend/HoReD/Controllers/DoctorController.cs
+++ backend/HoReD/Controllers/DoctorController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web.Http;
+using System.Web.Http.Results;
 using Entities;
 using Entities.Services;
 
@@ -25,33 +26,33 @@ namespace HoReD.Controllers
         /// <returns>List of instances of the class DoctorInfo</returns>
         /// <example>http://localhost:*****/api/Doctor/</example>
         [HttpGet]
-        public List<DoctorInfo> GetDoctors()
+        public IHttpActionResult GetDoctors()
         {
-            return _doctorService.GetDoctors();
+            return Ok(_doctorService.GetDoctors());
         }
 
         [HttpGet]
         [Route("GetDoctors/{professionId}")]
-         public List<string[]> GetDoctorsByProfession(int professionId)
+         public IHttpActionResult GetDoctorsByProfession(int professionId)
         {
-             return _doctorService.GetDoctorsByProfessionId(professionId);
+             return Ok(_doctorService.GetDoctorsByProfessionId(professionId));
         }
 
         [HttpGet]
         [ActionName("GetProfessions")]
         [Route("ProfessionsStatic/{isStatic=true}")]
         [Route("ProfessionsNotStatic/{isStatic=false}")]
-        public List<string[]> GetProfessions(bool isStatic)
+        public IHttpActionResult GetProfessions(bool isStatic)
         {
-            return _doctorService.GetProfessions(isStatic);
+            return Ok(_doctorService.GetProfessions(isStatic));
         }
 
         [HttpGet]
         [Route("DoctorEvents/{doctorId}/{dateStart}/{dateFinish}")]
-        public List<string[]> GetDoctorEvents(int doctorId,DateTime dateStart,DateTime dateFinish)
+        public IHttpActionResult GetDoctorEvents(int doctorId,DateTime dateStart,DateTime dateFinish)
         {
             var rules = _doctorService.GetDoctorAllRules(doctorId, dateStart, dateFinish);
-            return _doctorService.ConvertToEvents(rules, dateStart, dateFinish);
+            return Ok(_doctorService.ConvertToEvents(rules, dateStart, dateFinish));
         }
 
     }
--- backend/HoReD/Controllers/DoctorScheduleController.cs
+++ backend/HoReD/Controllers/DoctorScheduleController.cs
@@ -29,9 +29,9 @@ namespace HoReD.Controllers
 
         [HttpGet]
         [Route("GetDoctorSchedule/{doctorId}")]
-        public List<string[]> GetDoctorSchedule(int doctorId)
+        public IHttpActionResult GetDoctorSchedule(int doctorId)
         {
-            return _doctorService.GetDoctorSchedule(doctorId);
+            return Ok(_doctorService.GetDoctorSchedule(doctorId));
         }
 
     }
