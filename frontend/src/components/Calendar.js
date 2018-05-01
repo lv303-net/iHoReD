5d657912d4725fbb07c3eb0648a5079dde21fd7a
IvanovVadym <vadym.ivanov@ukr.net>
2018-05-01 13:26:40 +0300

    changed type of showing events on month view

--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -31,17 +31,6 @@ class Calendar extends React.Component{
         axios.post(server_url + '/api/Schedule',bookingEvent);
       }
 
-    // handleSubmitBooking = event => {
-    //   var bookingEvent = {
-    //     IdDoctor: this.state.idDoc,
-    //     IdPatient: localStorage.getItem("currentUserId"),
-    //     startDateTime: this.state.startTime,
-    //     endDateTime:this.state.endTime
-    //   }
-  
-    //     axios.post(server_url + '/api/Schedule',bookingEvent);
-    //   }
-
     saveCurrentDayStartEnd(start, end)
     {
         var url_string = window.location.href;
@@ -154,42 +143,45 @@ class Calendar extends React.Component{
       });
     }
 
-    Events(newEvents, isMonth)
+    addEvents(newEvents, isMonth)
     {
-    //   if(isMonth)
-    //     {
-    //       $($('#calendar').fullCalendar('getView').el[0]).find('.fc-day[data-date=' + newstart.slice(0, 10)+ ']').css('background-color', 'red');
-    //     }
+      if(isMonth)
+        {
+          newEvents.map(event => {
+          $($('#calendar').fullCalendar('getView').el[0]).find('.fc-day[data-date=' + event.start.slice(0, 10)+ ']').css('background-color', 'red');
+        })
+      }
+      else
          $('#calendar').fullCalendar( 'addEventSource', newEvents);
      }
 
-      addEvent(newstart, newend, isMonth) {
-        var event
-        var dayoff
-        if(isMonth)
-        {
-          $($('#calendar').fullCalendar('getView').el[0]).find('.fc-day[data-date=' + newstart.slice(0, 10)+ ']').css('background-color', 'red');
-        }
+    //   addEvent(newstart, newend, isMonth) {
+    //     var event
+    //     var dayoff
+    //     if(isMonth)
+    //     {
+    //       $($('#calendar').fullCalendar('getView').el[0]).find('.fc-day[data-date=' + newstart.slice(0, 10)+ ']').css('background-color', 'red');
+    //     }
           
-        else
-        {
-          event={
-            start  : newstart,
-            end  : newend,
-            color : "green",
-            blocked: false,
-          };
-          $('#calendar').fullCalendar( 'addEventSource', event);
-          dayoff={
-            start: newstart,
-              end: newend,
-              rendering: 'inverse-background',
-              color: "#DCDCDC"
-          },
+    //     else
+    //     {
+    //       event={
+    //         start  : newstart,
+    //         end  : newend,
+    //         color : "green",
+    //         blocked: false,
+    //       };
+    //       $('#calendar').fullCalendar( 'addEventSource', event);
+    //       dayoff={
+    //         start: newstart,
+    //           end: newend,
+    //           rendering: 'inverse-background',
+    //           color: "#DCDCDC"
+    //       },
          
-           $('#calendar').fullCalendar( 'addEventSource', dayoff);
-        }    
-    }
+    //        $('#calendar').fullCalendar( 'addEventSource', dayoff);
+    //     }    
+    // }
       shouldComponentUpdate(nextProps, nextState) {
         return ((this.state.startPeriod!== nextState.startPeriod) || (this.state.endPeriod!== nextState.endPeriod) 
         || (this.state.idDoc!== nextState.idDoc) || (this.state.startTime!== nextState.startTime) || (this.state.endTime!== nextState.endTime) );       
@@ -197,15 +189,6 @@ class Calendar extends React.Component{
 
       componentWillUpdate(nextProps, nextState)
       {
-        // $('#calendar').fullCalendar({
-        //   loading: function( isLoading, view ) {
-        //     if(isLoading) {// isLoading gives boolean value
-        //         alert("gggg") 
-        //     } else {
-        //         alert("ddddd")
-        //     }
-        // }
-        // })
         var getData = (this.state.startPeriod!== nextState.startPeriod) ||(this.state.endPeriod!== nextState.endPeriod) || (this.state.idDoc!== nextState.idDoc); 
           if(getData)
           {      
@@ -216,14 +199,8 @@ class Calendar extends React.Component{
             isMonth = true;
           else 
             isMonth = false;
-            // this.setState({
-            //   loading: true
-            // }, () =>
           axios.get(server_url+'/DoctorEvents/' + nextState.idDoc +'/' + nextState.startPeriod+'/' + nextState.endPeriod)
           .then(response => {
-              // response.data.map(event => {   
-              //   this.addEvent(event[0]+'T'+event[1], event[0]+'T'+event[2], isMonth) 
-              // })
             var col;
             if(isMonth)
               col="red"
@@ -239,38 +216,16 @@ class Calendar extends React.Component{
             }
           })
           console.log(building);
-          this.Events(building, isMonth);
-            // this.setState({
-            //   events: response.data,
-            //   loading: false
-            // }, () =>
-            // this.state.events.map(event => {   
-            //   this.addEvent(event[0]+'T'+event[1], event[0]+'T'+event[2], isMonth) 
-            // }))
-                
-                // ,
-                // this.setState({loading: false});
-              //}))
-              //this.setState({loading: false});
-              //this.setState({loading: false});
-              //});
-            
+          this.addEvents(building, isMonth);   
           })
-          //this.setState({loading: false})
       }
     }
 
     render(){
       let content;
-      // const { loading } = this.state;
-      // if(this.state.loading)
-      //   content = <Loader/>
-      // else
-      // {
         content = 
         <div>
         <div id = "calendar">
-        {/* need button because issue related with opening modal from fullcalendar */}
           <button data-toggle="modal" data-target="#mModal" id = "modButton" style={{display: "none"}}>
           </button>
           </div>
@@ -281,7 +236,6 @@ class Calendar extends React.Component{
               <h4 className="modal-title" id="mModalLabel">Confirm your booking</h4>
                 <button type="button" className="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span className="sr-only">Close</span></button> 
               </div>
-              {/* <form className="ml-3 mr-3" > */}
               <div className="modal-body">
                 DoctorId - {this.state.idDoc}<br/>
                 Start - {this.state.startTime}<br/>
@@ -291,12 +245,10 @@ class Calendar extends React.Component{
                 <button type="button" className="btn btn-danger btn-lg" data-dismiss="modal">Close</button>
                 <button type="button" className="btn btn-info btn-lg" data-dismiss="modal" onClick={() =>{this.handleSubmitBooking()}}>Confirm booking</button>
               </div>
-            {/* </form> */}
           </div>
         </div>
         </div>
         </div>
-          //}
           return <div>{content}</div>
   }
   }
