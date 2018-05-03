6f32251b8621d56f03a3f5cc399cfbd2ee860e00
Shkolyk <33028375+Shkolyk@users.noreply.github.com>
2018-05-03 11:36:25 +0200

    Added footer

--- frontend/src/components/Footerbar.js
+++ frontend/src/components/Footerbar.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import { Component } from 'react';
+import '../style/Footer.css';
 
 class Footerbar extends Component{
      render() {
         return(
             <div>
-                <p className='font-italic bg-secondary text-white text-center'>Ніхто ще не скаржився на наш сервіс!</p>
+                <section class="cid-qv5AKsQ6xL" id="footer2-3g" data-rv-view="4075">
+                    <div class="container">
+                        <div class="media-container-row content mbr-white text-center row">
+                            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                                <p class="mbr-text">
+                                    <strong>Address</strong>
+                                    <br/>
+                                    <br/>Ivan Mykolaichuk Street, 9
+                                    <br/>Lviv, 79000
+                                </p>
+                            </div>
+                            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                                <p class="mbr-text">
+                                    <strong>Contacts</strong>
+                                    <br/>
+                                    <br/>Email: hospital.lv303@gmail.com
+                                    <br/>Phone: 0322 527 011
+                                </p>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="google-map"><iframe frameborder="0" style={{border: 0}} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2571.3114564247403!2d24.037428315141177!3d49.87417737940008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473adc57dc4191e7%3A0x2cc24ce997f893e!2z0LLRg9C70LjRhtGPINCG0LLQsNC90LAg0JzQuNC60L7Qu9Cw0LnRh9GD0LrQsCwgOSwg0JvRjNCy0ZbQsiwg0JvRjNCy0ZbQstGB0YzQutCwINC-0LHQu9Cw0YHRgtGMLCA3OTAwMA!5e0!3m2!1suk!2sua!4v1525338801787" allowfullscreen></iframe></div>
+                            </div>
+                        </div>
+                        <div class="footer-lower">
+                            <div class="media-container-row">
+                                <div class="col-sm-12">
+                                    <hr></hr>
+                                </div>
+                            </div>
+                            <div class="media-container-row mbr-white">
+                                <div class="col-sm-12">
+                                    <p class="mbr-text mbr-fonts-style copyrith-font text-center m-0 mt-5">
+                                        © HoReD Lv-303.Net   "Ніхто ще не скаржився на наш сервіс!"
+                                    </p>
+                                </div>                               
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
             </div>
         );
     }
