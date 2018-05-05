94bbd604da9f87b1427caa46514054f94aa52b69
usyktt <32979014+usyktt@users.noreply.github.com>
2018-05-06 00:42:34 +0300

    Update V3.24_PROCEDURE_FOR_SALARY_STATISTIC.sql

--- db/V3.24_PROCEDURE_FOR_SALARY_STATISTIC.sql
+++ db/V3.24_PROCEDURE_FOR_SALARY_STATISTIC.sql
@@ -1,7 +1,7 @@
-GO
+
 IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'GET_WORKED_HOURS')
 	DROP PROC GET_WORKED_HOURS
-
+GO
 CREATE PROCEDURE GET_WORKED_HOURS
 	@DOCTOR_ID INT,
 	@DATE DATETIME,
@@ -11,11 +11,12 @@ BEGIN
 	SET @WORKED_HOURS = (SELECT COALESCE(SUM(VISIT_TIME)/60., 0) FROM (SELECT (DATEDIFF(MINUTE, START_DATETIME, END_DATETIME)) AS VISIT_TIME FROM SCHEDULE 
 	WHERE DAY(START_DATETIME) = DAY(@DATE) AND IDDOCTOR = @DOCTOR_ID) TOTAL_TIME)
 END;
+GO
 -----------------------------------------------------------------------------------------------------
-GO									   
+									   
 IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'GET_SALARY_RATE')
 	DROP PROC GET_SALARY_RATE
-
+GO
 CREATE PROCEDURE GET_SALARY_RATE
 	@DOCTOR_ID INT,
 	@DATE DATETIME,
@@ -25,11 +26,12 @@ BEGIN
 	SET @RATE = (SELECT TOP 1 RATE FROM SALARY_RATES INNER JOIN DOCTORS ON SALARY_RATES.PROFFESION_ID=DOCTORS.IDPROFESSION
 	WHERE PERIOD_START <= @DATE AND DOCTORS.IDDoctors = @DOCTOR_ID)
 END;
+GO
 ------------------------------------------------------------------------------------------
-GO									   
+									   
 IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'GET_SALARY_COEFFICIENT')
 	DROP PROC GET_SALARY_COEFFICIENT
-
+GO
 CREATE PROCEDURE GET_SALARY_COEFFICIENT
 	@DOCTOR_ID INT,
 	@DATE DATETIME,
@@ -40,11 +42,12 @@ BEGIN
 	WHERE PERIOD_START <= @DATE AND DOCTORS.IDDoctors = @DOCTOR_ID
 	ORDER BY PERIOD_START DESC)
 END;
+GO
 --------------------------------------------------------------------------------------------------------------------------
-GO									   
+									   
 IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'GET_SALARY_STATISTICS_FOR_PERIOD')
 	DROP PROC GET_SALARY_STATISTICS_FOR_PERIOD
-
+GO
 CREATE PROCEDURE GET_SALARY_STATISTICS_FOR_PERIOD
 	@DOCTOR_ID INT,
 	@START_DATE DATETIME,
@@ -60,15 +63,13 @@ BEGIN
 		SET @SPECIFIS_DAY = DATEADD(DAY, 1, @SPECIFIS_DAY)
 	END;
 	SELECT * FROM @TEMP
-END;									   
-									   
-									   
-									   
+END; 
+GO									  							   
 -------------------------------------------------------------------------------------------
-GO																     
+																     
 IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'GET_SALARY_STATISTICS_FOR_DAY')
 	DROP PROC GET_SALARY_STATISTICS_FOR_DAY
-
+GO
 CREATE PROCEDURE GET_SALARY_STATISTICS_FOR_DAY
 	@DOCTOR_ID INT,
 	@DATE DATETIME
@@ -86,3 +87,4 @@ BEGIN
 
 	SELECT @DATE, @WORKED_HOURS, @RATE, @COEFFICIENTS, @WORKED_HOURS * @RATE * @COEFFICIENTS
 END;
+GO
