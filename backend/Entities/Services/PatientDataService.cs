19a0d3f0cba8c8a0d87385ec6c9f3f49d1572a06
IvanovVadym <vadym.ivanov@ukr.net>
2018-05-15 11:23:45 +0300

    added parameters to procedures for diagnoses

--- backend/Entities/Services/PatientDataService.cs
+++ backend/Entities/Services/PatientDataService.cs
@@ -75,8 +75,7 @@ namespace Entities.Services
         public List<IllnessCategory> GetCategories()
         {
             string cmd = "RETURN_CATEGORIES";
-            var param = new Dictionary<string, object>()
-            { };
+            var param = new Dictionary<string, object>();
 
             try
             {
@@ -93,8 +92,9 @@ namespace Entities.Services
         {
             string cmd = "RETURN_SUBCATEGORIES";
             var param = new Dictionary<string, object>()
-            { };
-
+            {
+                {"@CATEGORY", idCategory }
+            };
             try
             {
                 var data = _dbContext.ExecuteSqlQuery(cmd, '*', param);
@@ -110,7 +110,9 @@ namespace Entities.Services
         {
             string cmd = "RETURN_DISEASES";
             var param = new Dictionary<string, object>()
-            { };
+            {
+                {"@SUBCATEGORY", idSubCategory }
+            };
 
             try
             {
