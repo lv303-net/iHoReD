f0f609df0511ae4169f6c3c5c9e6f352afe0ac6c
ira-halenok <iryna.halenok@gmail.com>
2018-05-15 19:07:14 +0300

    Added basic data for user_info and allergies tables

---
+++ db/V3.64_INSERT_ALLERGIES_TABLE.sql
@@ -0,0 +1,9 @@
+BEGIN TRANSACTION;
+INSERT INTO ALLERGIES 'Mushrooms', 'Mushrooms allergy description'
+INSERT INTO ALLERGIES 'Oranges', 'Oranges allergy description'
+INSERT INTO ALLERGIES 'Dust', 'Dust allergy description'
+INSERT INTO ALLERGIES 'Penicillin', 'Penicillin allergy description'
+INSERT INTO ALLERGIES 'Wool', 'Wool allergy description'
+INSERT INTO ALLERGIES 'Mildew', 'Mildew allergy description'
+INSERT INTO ALLERGIES 'Pollen', 'Pollen allergy description'
+COMMIT TRANSACTION;
