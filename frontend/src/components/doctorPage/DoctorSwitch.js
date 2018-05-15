999d84788ac48d9d4745c29b833b0893bc9549d9
IvanovVadym <vadym.ivanov@ukr.net>
2018-05-15 16:03:29 +0300

    added select fields for chppse category and subcategory

--- frontend/src/components/doctorPage/DoctorSwitch.js
+++ frontend/src/components/doctorPage/DoctorSwitch.js
@@ -6,6 +6,7 @@ import DoctorCalendar from './../doctorPage/DoctorCalendar'
 import NotFound from './../NotFound'
 import '../../style/DoctorPage.css'
 import PatientMedicalCard from './PatientMedicalCard/PatientMedicalCard'
+import Diagnoses from './PatientMedicalCard/PatientDiseses/Diagnoses'
 
 class DoctorSwitch extends Component {
   render() {
@@ -17,6 +18,7 @@ class DoctorSwitch extends Component {
             <Route exact path="/doctor/mySchedule" component={DoctorCalendar}/>
             <Route exact path="/doctor/patientMedicalCard/:id" component={PatientMedicalCard}/>
             <Route exact path="/doctor/salary" />
+            <Route exact path="/doctor/patientDiseases" component={Diagnoses}/>
             <Route component={NotFound} />
           </Switch>
         </main>
