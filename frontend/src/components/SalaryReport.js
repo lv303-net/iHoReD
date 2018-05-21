6a0f3a13a5cefdbfcef96e839bd7acb345eb427c
usyktt <usyktt@gmail.com>
2018-05-21 14:20:44 +0300

    Salary report add collapse+ add method DoctorGeneralSalaryStatistics

--- frontend/src/components/SalaryReport.js
+++ frontend/src/components/SalaryReport.js
@@ -23,12 +23,14 @@ class SalaryReport extends React.Component {
     var url_string = window.location.href;
     var url = new URL(url_string);
     if (url.search !== '') {
-      var startdate = Number(url.searchParams.get("startdate"));
-      var enddate = Number(url.searchParams.get("enddate"));
+      var startdate =url.searchParams.get("startdate");
+         var enddate =url.searchParams.get("enddate");
+         this.state.startDate=moment(startdate); 
+         this.state.endDate=moment(enddate); 
          //axios.get(localStorage.getItem("server_url") + '/DoctorSalaryStatistics/' + this.props.match.params.id + '/' +this.state.startDate.format('YYYY-MM-DD')+ '/' +this.state.endDate.format('YYYY-MM-DD'))
          axios({
           method: 'get',
-          url: localStorage.getItem("server_url") + '/DoctorSalaryStatistics/' + this.props.match.params.id + '/' +this.state.startDate.format('YYYY-MM-DD')+ '/' +this.state.endDate.format('YYYY-MM-DD'),
+          url: localStorage.getItem("server_url") + '/DoctorSalaryStatistics/' +  this.props.match.params.id + '/' + startdate+ '/' +enddate,
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             // 'Authorization': 'Bearer ' + localStorage.getItem("accessToken")
@@ -40,6 +42,20 @@ class SalaryReport extends React.Component {
         })
 
       });
+
+      axios({
+        method: 'get',
+        url:localStorage.getItem("server_url") + '/DoctorGeneralSalaryStatistics/' + this.props.match.params.id + '/' + startdate+ '/' +enddate,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          // 'Authorization': 'Bearer ' + localStorage.getItem("accessToken")
+        }
+      })
+           .then(rez => {
+            this.setState({            
+              totaldata: rez.data     
+                 })
+               });        
     }
     else {
       var searchParameter = new URLSearchParams(window.location.search);
@@ -60,7 +76,22 @@ class SalaryReport extends React.Component {
             salaryData: res.data
           })
         });
-    }
+   
+    axios({
+      method: 'get',
+      url:localStorage.getItem("server_url") + '/DoctorGeneralSalaryStatistics/' + this.props.match.params.id + '/' +this.state.startDate.format('YYYY-MM-DD')+ '/' +this.state.endDate.format('YYYY-MM-DD'),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        // 'Authorization': 'Bearer ' + localStorage.getItem("accessToken")
+      }
+    })
+    .then(rez => {
+     this.setState({            
+     totaldata: rez.data     
+         })
+       }); 
+      }         
+       
     this.handleChangeStart = this.handleChangeStart.bind(this);
     this.handleChangeEnd = this.handleChangeEnd.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -98,6 +129,20 @@ class SalaryReport extends React.Component {
           salaryData: res.data,
         })
       });
+      axios({
+        method: 'get',
+        url:localStorage.getItem("server_url") + '/DoctorGeneralSalaryStatistics/' + this.props.match.params.id +  '/' + start + '/' + end,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          // 'Authorization': 'Bearer ' + localStorage.getItem("accessToken")
+        }
+      })
+  .then(rez => {
+   this.setState({            
+     totaldata: rez.data     
+        })
+
+      });  
     $('#multiCollapse').attr('aria-expanded', 'false');
     $('#multiCollapse').addClass('collapsed');
     $('#multiCollapseExample1').removeClass('show');
@@ -110,49 +155,18 @@ class SalaryReport extends React.Component {
     searchParameter.set('enddate', end);
     window.history.pushState(null, null, `${window.location.pathname}?${searchParameter.toString()}${window.location.hash}`);
   }
-  sumWorkedHours() {
-    let array = this.state.salaryData.map(items => items.map(item => item.WorkedHours));
-    let total = 0;
-    for (let i = 0; i < array.length; i++) {
-
-      total += array[i][array[i].length - 1];
-
-    }
-    return total;
-  }
-  averageCoeff() {
-    let array = this.state.salaryData.map(items => items.map(item => item.SalaryCoefficient));
-    let total = 0;
-    for (let i = 0; i < array.length; i++) {
-      total += array[i][array[i].length - 1] / array.length;
-    }
-    return Math.round(total, 4);
-  }
-  averageRate() {
-    let array = this.state.salaryData.map(items => items.map(item => item.SalaryRate))
-    let total = 0;
-    for (let i = 0; i < array.length; i++) {
-      total += array[i][array[i].length - 1] / array.length;
-    }
-    return Math.round(total, 4);
-  }
-  earnedMoney() {
-    let array = this.state.salaryData.map(items => items.map(item => item.EarnedMoney))
-    let total = 0;
-    for (let i = 0; i < array.length; i++) {
-      total += array[i][array[i].length - 1];
-    }
-    return total;
-  }
-  render() {
-    return (
+   render() {
+    let listitems = this.state.salaryData.map(items =><li><a className="btn btn-primary salarybutton mx-1"
+     data-toggle="collapse" id="multiCollapse" href={'#' + items[0].Day.toString()} role="button" aria-expanded="true"
+    aria-controls="multiCollapseExample">{items[0].Day.slice(0,7).toString()}</a></li>); 
+      return (
       <div>
         <div className="panel-body text-center py-5 mx-5">
-          <h2 id="name" className="font-italic text-center text-muted">
+        <h2 id="name" className="font-italic text-center text-muted">
             Salary Report
           </h2>
-          <h3 id="name">
-            {localStorage.getItem("currentUserFirstName") + "    "}
+          <h3 id="name" className="font-italic text-center text-muted">
+            {localStorage.getItem("currentUserFirstName") + "    "} 
             {" " + localStorage.getItem("currentUserLastName")}
           </h3>
         </div>
@@ -161,10 +175,10 @@ class SalaryReport extends React.Component {
             <div className="col-lg-4 col-md-4 col-12">
               Start Date:
               <DatePicker
-                selected={this.state.startDate}
+                selected={this.state.startDate}        
                 selectsStart
                 dateFormat="YYYY-MM-DD"
-                startDate={this.state.startDate}
+                startDate={this.state.startDate}  
                 onChange={this.handleChangeStart}
                 maxDate={this.state.endDate}
               />
@@ -176,8 +190,8 @@ class SalaryReport extends React.Component {
                 dateFormat="YYYY-MM-DD"
                 maxDate={moment()}
                 minDate={this.state.startDate}
-                endDate={this.state.endDate}
-                onChange={this.handleChangeEnd}
+                endDate={this.state.endDate}  
+                 onChange={this.handleChangeEnd}
               />
             </div>
             <div className="col-lg-4 col-md-4 col-12 mt-3">
@@ -186,65 +200,59 @@ class SalaryReport extends React.Component {
           </div>
           <div className="row text-center">
             <div className="col-12">
-              <div className="row mt-5 mx-1">
-                <div className="col">
-                  <div className="row" id="patientcard">
+                   <div className="row mt-5 mx-1">
+                  <div className="col">
+                    <div className="row" id="patientcard">
                     <div className="col-6 col-custom-header" id="col-custom">Total sum</div>
-                    <div className="col-6">{this.earnedMoney()}</div>
-                  </div>
-                  <div className="row" id="patientcard">
-                    <div className="col-6 col-custom-header" id="col-custom">Average rate</div>
-                    <div className="col-6">{this.averageRate()}</div>
-                  </div>
-                  <div className="row" id="patientcard">
-                    <div className="col-6 col-custom-header" id="col-custom">Average coefficient</div>
-                    <div className="col-6">{this.averageCoeff()}</div>
-                  </div>
-                  <div className="row" id="patientcard">
-                    <div className="col-6 col-custom-header" id="col-custom">Worked hours</div>
-                    <div className="col-6">{this.sumWorkedHours()}</div>
+                      <div className="col-6">{this.state.totaldata.EarnedMoney}</div>                                   
+                    </div>
+                    <div className="row" id="patientcard">
+                      <div className="col-6 col-custom-header" id="col-custom">Average rate</div>
+                      <div className="col-6">{this.state.totaldata.SalaryRate}</div>
+                    </div>
+                    <div className="row" id="patientcard">
+                      <div className="col-6 col-custom-header" id="col-custom">Average coefficient</div>
+                      <div className="col-6">{this.state.totaldata.SalaryCoefficient}</div>
+                    </div>
+                    <div className="row" id="patientcard">
+                      <div className="col-6 col-custom-header" id="col-custom">Worked hours</div>
+                      <div className="col-6">{this.state.totaldata.WorkedHours}</div>
+                    </div>
                   </div>
                 </div>
-              </div>
             </div>
             <div className="col-3 mt-5">
-              <p><a class="btn btn-primary salarybutton" data-toggle="collapse" id="multiCollapse" href="#collapseExample1" role="button" aria-expanded="false"
+              <p><a className="btn btn-primary salarybutton mx-1" data-toggle="collapse" id="multiCollapse" href="#collapse1" role="button" aria-expanded="false"
                 aria-controls="multiCollapseExample1">More details</a></p>
             </div>
-          </div>
-
-          <div class="collapse multi-collapse mt-5" id="collapseExample1">
-
-
-            {this.state.salaryData.map(items =>
-              <div className="monthButton">
-                <div className="row">
-                  <div className="col-3 col-md-4">
-                    <p><a className="btn btn-primary salarybutton" data-toggle="collapse" id="multiCollapse" href={'#' + items.toString()} role="button" aria-expanded="true"
-                      aria-controls="multiCollapseExample">{items[0].Day.slice(0, 7).toString()}</a></p>
-                  </div>
-                </div>
-                <div className="collapse multi-collapse mt-5 monthInfo" id={items.toString()}>
-                  <div className="row" id="patientcard">
-                    <div className="col-3 col-custom-header" id="col-custom">Date</div>
-                    <div className="col-2 col-custom-header" id="col-custom">Hours</div>
-                    <div className="col-2 col-custom-header" id="col-custom">Coeff</div>
-                    <div className="col-2 col-custom-header" id="col-custom">Rate</div>
-                    <div className="col-3 col-custom-header">Total</div>
-                  </div>
-                  {
-                    items.map(item => <DayReport day={item.Day}
-                      workedHours={item.WorkedHours} salaryCoefficient={item.SalaryCoefficient}
-                      salaryRate={item.SalaryRate} earnedMoney={item.EarnedMoney}
-                    />)
-                  }
-                </div>
+          </div>        
+            <div className="collapse multi-collapse mt-5" id="collapse1">          
+            <ul className="nav nav-tabs">{listitems}</ul>
+            <div className="monthButton mx-3"> 
+                {this.state.salaryData.map(items =>      
+              <div className="collapse multi-collapse mt-5 monthInfo" id={items[0].Day.toString()}>
+              <div className="row" id="patientcard">
+                <div className="col-3 col-custom-header" id="col-custom">Date</div>
+                <div className="col-2 col-custom-header" id="col-custom">Hours</div>
+                <div className="col-2 col-custom-header" id="col-custom">Coeff</div>
+                <div className="col-2 col-custom-header" id="col-custom">Rate</div>
+                <div className="col-3 col-custom-header">Total</div>
+              </div>             
+               {
+                items.map(item=> <DayReport  day={item.Day}
+                  workedHours={item.WorkedHours} salaryCoefficient={item.SalaryCoefficient}
+                  salaryRate={item.SalaryRate} earnedMoney={item.EarnedMoney}
+                />)
+                 }
+                 </div>               
+              )}
               </div>
-            )}
-          </div>
+              </div>                     
         </div>
-      </div>
+     </div>
+
     )
   }
 }
 export default SalaryReport
+
