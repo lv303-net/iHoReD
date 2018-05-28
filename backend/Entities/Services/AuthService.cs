b4677d36e4a5fa4bf2e880367403ef184e0bb4b2
olyasol <solyarolya@gmail.com>
2018-05-29 00:13:21 +0300

    Update AuthService.cs

--- backend/Entities/Services/AuthService.cs
+++ backend/Entities/Services/AuthService.cs
@@ -19,31 +19,6 @@ namespace Entities.Services
             _rsaProvider = rsaProvider;
         }
 
-        //public static string GenerateToken(string username,string role, int expireMinutes = 20)
-        //{
-        //    var symmetricKey = Convert.FromBase64String(Secret);
-        //    var tokenHandler = new JwtSecurityTokenHandler();
-
-        //    var now = DateTime.UtcNow;
-        //    var tokenDescriptor = new SecurityTokenDescriptor
-        //    {
-        //        Subject = new ClaimsIdentity(new[]
-        //                {
-        //                new Claim(ClaimTypes.Name, username),
-        //                new Claim(ClaimTypes.Role,role)
-        //            }),
-
-        //        Expires = now.AddMinutes(Convert.ToInt32(expireMinutes)),
-
-        //        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(symmetricKey), SecurityAlgorithms.HmacSha256Signature)
-        //    };
-
-        //    var stoken = tokenHandler.CreateToken(tokenDescriptor);
-        //    var token = tokenHandler.WriteToken(stoken);
-
-        //    return token;
-        //}
-
         public string GenerateJwtTokenAsync(string userEmail, string password)
         {
             if (!_membershipProvider.VerifyUserPassword(userEmail, password))
