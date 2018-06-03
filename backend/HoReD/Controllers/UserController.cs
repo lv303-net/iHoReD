8169d706c6c89df8314d8a445e38d87f92c578a8
olyasol <solyarolya@gmail.com>
2018-06-03 22:39:57 +0300

    Update UserController.cs

--- backend/HoReD/Controllers/UserController.cs
+++ backend/HoReD/Controllers/UserController.cs
@@ -74,8 +74,7 @@ namespace HoReD.Controllers
         /// <param name="firstOrlastname">firstOrlastname(unnecessary param)</param>
         /// <returns>List of filtered info about users-first and lastname,isAdmin,profession</returns>
         [HttpGet]
-        [AllowAnonymous]
-        //[TokenAuthenticate(Role = "admin")]
+        [TokenAuthenticate(Role = "admin")]
         [Route("FilterAllUsers/{numberPage}/{countInPage}/{isAdmin}/{isDoctor}")]
         [Route("FilterAllUsers/{numberPage}/{countInPage}/{isAdmin}/{isDoctor}/{firstOrlastname}")]
         public IHttpActionResult FilterAllUsers(int numberPage, int countInPage,bool isAdmin, bool isDoctor, string firstOrlastname=null)
@@ -128,8 +127,7 @@ namespace HoReD.Controllers
         /// <param name="firstOrlastname">firstOrlastname(unnecessary param)</param>
         /// <returns>Count of pages(for pagination)</returns>
         [HttpGet]
-        [AllowAnonymous]
-        //[TokenAuthenticate(Role = "admin")]
+        [TokenAuthenticate(Role = "admin")]
         [Route("NumbersOfPageFiltered/{countInPage}/{isAdmin}/{isDoctor}")]
         [Route("NumbersOfPageFiltered/{countInPage}/{isAdmin}/{isDoctor}/{firstOrlastname}")]
         public IHttpActionResult NumbersOfPageFiltered(int countInPage,bool isAdmin, bool isDoctor, string firstOrLastname=null)
